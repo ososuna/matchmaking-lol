088e3f68df2ef6663150e07dffe6ffcacb6a9f9d
ososuna <oswaldo.adrian@live.com.mx>
2021-02-28 21:55:19 -0700

    Matchmaking LoL

---
+++ classes.py
@@ -0,0 +1,321 @@
+
+class Player:
+
+    def __init__(self, user, level, winrate, division, rank, autofill, primary_lane, secondary_lane):
+        self.__user = user
+        self.__level = level
+        self.__winrate = winrate
+        self.__division = division
+        self.__rank = rank
+        self.__autofill = autofill
+        self.__primary_lane = primary_lane
+        self.__secondary_lane = secondary_lane
+        self.__mmr = self.calculate_mmr()
+
+    @property
+    def user(self): 
+        return self.__user
+    
+    @property
+    def level(self): 
+        return self.__level
+    
+    @property
+    def winrate(self): 
+        return self.__winrate
+    
+    @property
+    def division(self): 
+        return self.__division
+    
+    @property
+    def rank(self): 
+        return self.__rank
+
+    @property
+    def autofill(self): 
+        return self.__autofill
+    
+    @property
+    def primary_lane(self): 
+        return self.__primary_lane
+    
+    @property
+    def secondary_lane(self): 
+        return self.__secondary_lane
+    
+    @property
+    def mmr(self): 
+        return self.__mmr
+    
+    @mmr.setter 
+    def mmr(self, mmr):
+        self.__mmr = mmr
+
+    def calculate_mmr(self):
+        if self.division == 'Iron':
+            self.mmr = 200
+            if self.rank == 1:
+                self.mmr += 400
+            elif self.rank == 2:
+                self.mmr += 300
+            elif self.rank == 3:
+                self.mmr += 200
+            elif self.rank == 4:
+                self.mmr += 100
+            self.mmr += (self.mmr*.1) * (self.winrate*.01)
+        elif self.division == 'Bronze':
+            self.mmr = 800
+            if self.rank == 1:
+                self.mmr += 160
+            elif self.rank == 2:
+                self.mmr += 120
+            elif self.rank == 3:
+                self.mmr += 80
+            elif self.rank == 4:
+                self.mmr += 40
+        elif self.division == 'Silver':
+            self.mmr = 1000
+            if self.rank == 1:
+                self.mmr += 160
+            elif self.rank == 2:
+                self.mmr += 120
+            elif self.rank == 3:
+                self.mmr += 80
+            elif self.rank == 4:
+                self.mmr += 40
+        elif self.division == 'Gold':
+            self.mmr = 1200
+            if self.rank == 1:
+                self.mmr += 200
+            elif self.rank == 2:
+                self.mmr += 150
+            elif self.rank == 3:
+                self.mmr += 100
+            elif self.rank == 4:
+                self.mmr += 50
+        elif self.division == 'Platinum':
+            self.mmr = 1500
+            if self.rank == 1:
+                self.mmr += 200
+            elif self.rank == 2:
+                self.mmr += 150
+            elif self.rank == 3:
+                self.mmr += 100
+            elif self.rank == 4:
+                self.mmr += 50
+        elif self.division == 'Diamond':
+            self.mmr = 1800
+            if self.rank == 1:
+                self.mmr += 240
+            elif self.rank == 2:
+                self.mmr += 180
+            elif self.rank == 3:
+                self.mmr += 120
+            elif self.rank == 4:
+                self.mmr += 60
+        elif self.division == 'Master':
+            self.mmr = 2200
+        elif self.division == 'Grand Master':
+            self.mmr = 2400
+        elif self.division == 'Challenger':
+            self.mmr = 2600
+        
+        if self.winrate >= 50:
+            self.mmr += (self.mmr*.2) * (self.winrate*.01)
+        else:
+            self.mmr -= (self.mmr*.2) * ((50 - self.winrate) *.01)
+        
+        return self.mmr
+    
+    def __str__(self):
+        return f'\n{self.user}\n{self.division} {self.rank}\nLevel {self.level}\nWin Rate: {self.winrate}%\nPrimary lane: {self.primary_lane}\nSecondary lane: {self.secondary_lane}'
+
+class Team:
+
+    def __init__(self, id):
+        self.__id = id
+        self.__top = None
+        self.__jg = None
+        self.__mid = None
+        self.__adc = None
+        self.__sup = None
+        self.__avg_mmr = None
+    
+    @property
+    def id(self): 
+        return self.__id
+    
+    @property
+    def top(self): 
+        return self.__top
+
+    @top.setter 
+    def top(self, top):
+        self.__top = top
+    
+    @property
+    def jg(self): 
+        return self.__jg
+
+    @jg.setter 
+    def jg(self, jg):
+        self.__jg = jg
+
+    @property
+    def mid(self): 
+        return self.__mid
+
+    @mid.setter 
+    def mid(self, mid):
+        self.__mid = mid
+    
+    @property
+    def adc(self): 
+        return self.__adc
+
+    @adc.setter 
+    def adc(self, adc):
+        self.__adc = adc
+    
+    @property
+    def sup(self): 
+        return self.__sup
+
+    @sup.setter 
+    def sup(self, sup):
+        self.__sup = sup
+    
+    @property
+    def avg_mmr(self): 
+        return self.__avg_mmr
+    
+    @avg_mmr.setter 
+    def avg_mmr(self, avg_mmr):
+        self.__avg_mmr = avg_mmr
+
+    def calculate_avg_mmr(self):
+        if self.top and self.jg and self.mid and self.adc and self.sup:
+            total_mmr = self.top.mmr + self.jg.mmr + self.mid.mmr + self.adc.mmr + self.sup.mmr
+            self.avg_mmr = total_mmr / 5
+            return self.avg_mmr
+    
+    def __str__(self):
+        return f'\ntop: {self.top.user}\njg: {self.jg.user}\nmid: {self.mid.user}\nadc: {self.adc.user}\nsup: {self.sup.user}'
+
+class Match:
+    
+    def __init__(self, id, red_team, blue_team):
+        self.__id = id
+        self.__red_team = red_team
+        self.__blue_team = blue_team
+    
+    @property
+    def id(self): 
+        return self.__id
+    
+    @property
+    def red_team(self): 
+        return self.__red_team
+
+    @red_team.setter 
+    def red_team(self, red_team):
+        self.__red_team = red_team
+
+    @property
+    def blue_team(self): 
+        return self.__blue_team
+
+    @blue_team.setter 
+    def blue_team(self, blue_team):
+        self.__blue_team = blue_team
+    
+    def __str__(self):
+        return f'\nRed Team\nAverage MMR: {self.red_team.calculate_avg_mmr()}{self.red_team}\n\nBlue Team\nAverage MMR: {self.blue_team.calculate_avg_mmr()}{self.blue_team}'
+
+class Link:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_end(self, newdata):
+        new_link = Link(newdata)
+        if self.head is None:
+            self.head = new_link
+            return
+        laste = self.head
+        while(laste.next):
+            laste = laste.next
+        laste.next=new_link
+
+    def remove(self, key):
+        head_value = self.head
+
+        if (head_value is not None):
+            if (head_value.value == key):
+                self.head = head_value.next
+                head_value = None
+                return
+
+        while (head_value is not None):
+            if head_value.value == key:
+                break
+            prev = head_value
+            head_value = head_value.next
+
+        if (head_value == None):
+            return
+
+        prev.next = head_value.next
+        head_value = None
+
+    def print_list(self):
+        printval = self.head
+        while printval is not None:
+            print (printval.value)
+            printval = printval.next
+    
+
+class Queue(SLinkedList):
+    
+    def __init__(self, elo):
+        super().__init__()    
+        self.server = 'LAN'
+        self.__elo = elo
+    
+    def form_team(self, team):
+        player = self.head
+        found_top = False
+        found_jg = False
+        found_mid = False
+        found_adc = False
+        found_sup = False
+        
+        while(player.next):
+            if player.value.primary_lane == 'top' and (not found_top):
+                team.top = player.value
+                found_top = True
+            elif player.value.primary_lane == 'jg' and (not found_jg):
+                team.jg = player.value
+                found_jg = True
+            elif player.value.primary_lane == 'mid' and (not found_mid):
+                team.mid = player.value
+                found_mid = True
+            elif player.value.primary_lane == 'adc' and (not found_adc):
+                team.adc = player.value
+                found_adc = True
+            elif player.value.primary_lane == 'sup' and (not found_sup):
+                team.sup = player.value
+                found_sup = True
+            
+            self.remove(player.value)
+
+            if found_top and found_jg and found_mid and found_adc and found_sup:
+                break
+            else:    
+                player = player.next
+
---
+++ main.py
@@ -0,0 +1,71 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from classes import *
+
+p1 = Player('pinguinodelanasa', 150, 55, 'Bronze', 1, True, 'adc', 'jg')
+p2 = Player('Charls Alonso', 146, 50, 'Bronze', 2, False, 'sup', 'top')
+p3 = Player('Johiliwisky18', 98, 58, 'Silver', 3, True, 'jg', 'mid')
+p4 = Player('nigaalan', 159, 56, 'Gold', 4, True, 'top', 'jg')
+p5 = Player('Osiris', 365, 60, 'Gold', 1, True, 'mid', 'adc')
+
+q1 = Queue('Silver')
+
+t1 = Team(1)
+
+users = ['Edgar', 'Pablo', 'Gerardo', 'Diego', 'Mariana', 'Carolina', 'Catherine', 'Gabo', 'Ivan', 'Luisa', 'Nahibi']
+lanes = ['top', 'jg', 'mid', 'adc', 'sup']
+mmr = 1350 + 320 * np.random.randn(100000)
+wr = 50 + 8 * np.random.randn(100000)
+plt.hist(mmr)
+
+def generate_rand_player():
+
+    rand_mmr = round(np.random.choice(mmr), 2)
+    rand_wr = round(np.random.choice(wr), 2)
+    rand_rank = random.randint(1,4)
+    rand_user = f'{np.random.choice(users)}{random.randint(0,9)}{random.randint(0,9)}'
+    rand_level = random.randint(30, 550)
+    rand_p_lane = np.random.choice(lanes)
+
+    while(True):
+        rand_s_lane = np.random.choice(lanes)
+        if rand_p_lane != rand_s_lane:
+            break
+    
+    if rand_mmr < 800:
+        p = Player(rand_user, rand_level, rand_wr, 'Iron', rand_rank, True, rand_p_lane, rand_s_lane)
+    elif 800 <= rand_mmr < 1000:
+        p = Player(rand_user, rand_level, rand_wr, 'Bronze', rand_rank, True, rand_p_lane, rand_s_lane)
+    elif 1000 <= rand_mmr < 1200:
+        p = Player(rand_user, rand_level, rand_wr, 'Silver', rand_rank, True, rand_p_lane, rand_s_lane)
+    elif 1200 <= rand_mmr < 1500:
+        p = Player(rand_user, rand_level, rand_wr, 'Gold', rand_rank, True, rand_p_lane, rand_s_lane)
+    elif 1500 <= rand_mmr < 1800:
+        p = Player(rand_user, rand_level, rand_wr, 'Platinum', rand_rank, True, rand_p_lane, rand_s_lane)
+    elif 1800 <= rand_mmr < 2200:
+        p = Player(rand_user, rand_level, rand_wr, 'Diamond', rand_rank, True, rand_p_lane, rand_s_lane)
+    elif 2200 <= rand_mmr < 2400:
+        p = Player(rand_user, rand_level, rand_wr, 'Master', None, True, rand_p_lane, rand_s_lane)
+    elif 2400 <= rand_mmr < 2600:
+        p = Player(rand_user, rand_level, rand_wr, 'Grand Master', None, True, rand_p_lane, rand_s_lane)
+    elif rand_mmr >= 2600:
+        p = Player(rand_user, rand_level, rand_wr, 'Challenger', None, True, rand_p_lane, rand_s_lane)
+    return p
+
+for i in range(0,10000):
+    p = generate_rand_player()
+    if 1000 <= p.mmr < 1200:
+        q1.insert_end(p)
+
+t1 = Team(1)
+t2 = Team(2)
+
+q1.form_team(t1)
+q1.form_team(t2)
+
+m1 = Match(1, t1, t2)
+print(m1)
+
+# %%
